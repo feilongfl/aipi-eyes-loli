d5c93a5699df5777568f7855aab200c6b4127024
YuLong Yao <feilongphone@gmail.com>
2023-12-16 12:30:13 +0800

    loli: load img to ram

--- AIPI-Eyes-Loli/loli.c
+++ AIPI-Eyes-Loli/loli.c
@@ -1,14 +1,27 @@
 #include "lcd.h"
 #include "loli_image.h"
 
-static inline int draw_picture_blocking(uint16_t x1, uint16_t y1, uint16_t x2,
-                                        uint16_t y2, lcd_color_t *picture) {
+#define LOLI_FRAME_X 240
+#define LOLI_FRAME_Y 280
+#define LOLI_FRAME_SIZE LOLI_FRAME_X * LOLI_FRAME_Y
+#define LOLI_FRAME_BYTESIZE LOLI_FRAME_SIZE * 2
+
+unsigned char loli_buffer_current = 0;
+unsigned short loli_buffer[LOLI_FRAME_SIZE] = {};
+
+static inline int loli_draw_picture(uint16_t x1, uint16_t y1, uint16_t x2,
+                                    uint16_t y2, lcd_color_t *picture) {
   return lcd_draw_picture_blocking(x1, y1, x2 - 1, y2 - 1, picture);
 }
 
+void loli_draw_frame(const unsigned short *frame) {
+  memcpy(loli_buffer, frame, LOLI_FRAME_BYTESIZE);
+  loli_draw_picture(0, 0, LOLI_FRAME_X, LOLI_FRAME_Y, loli_buffer);
+}
+
 void loli_draw() {
-  for (unsigned short i=0; loli_image[i]; i++) {
-    draw_picture_blocking(0, 0, 240, 280, loli_image[i]);
+  for (unsigned short i = 0; loli_image[i]; i++) {
+    loli_draw_frame(loli_image[i]);
   }
 }
 
