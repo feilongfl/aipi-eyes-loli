51655687325d1374aca3a3bf21fa98bb713d7505
YuLong Yao <feilongphone@gmail.com>
2023-12-16 12:55:43 +0800

    loli: fix image command comment

--- AIPI-Eyes-Loli/loli/command.sh
+++ AIPI-Eyes-Loli/loli/command.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# rm -rf image_*
+rm -rf image_*
 
-# mkdir image_origin
-# ffmpeg -i video.mp4 image_origin/video_%04d.png
+mkdir image_origin
+ffmpeg -i video.mp4 image_origin/video_%04d.png
 
-# mkdir -p image_crop
-# mkdir -p image_crop_240x280
-# mkdir -p image_header
-# # 循环遍历其他图片
-# for i in $(seq -f "%04g" 1 370)
-# do
-#   magick convert image_origin/video_$i.png -crop 360x420+0+170 image_crop/video_$i.png
-#   magick convert image_crop/video_$i.png -resize 240x280 image_crop_240x280/video_$i.png
+mkdir -p image_crop
+mkdir -p image_crop_240x280
+mkdir -p image_header
+# 循环遍历其他图片
+for i in $(seq -f "%04g" 1 370)
+do
+  magick convert image_origin/video_$i.png -crop 360x420+0+170 image_crop/video_$i.png
+  magick convert image_crop/video_$i.png -resize 240x280 image_crop_240x280/video_$i.png
 
-#   ./img2c.py image_crop_240x280/video_$i.png loli_image_$i image_header/loli_$i.h
-#   echo -ne "Progress: $i\r"
-# done
-# echo -e "Progress: $i\r"
+  ./img2c.py image_crop_240x280/video_$i.png loli_image_$i image_header/loli_$i.h
+  echo -ne "Progress: $i\r"
+done
+echo -e "Progress: $i\r"
 
 echo -e '// Auto Generate: ' > loli_image.h
 for i in $(seq -f "%04g" 1 370)
